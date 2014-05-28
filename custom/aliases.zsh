c40d54e76389e6c7cca93be6c90b78a6e2ff07a0
Dhaivat Pandit <dhaivat.pandit@sojern.com>
2014-05-27 22:13:09 -0700

    moving aliases to custom/

---
+++ custom/aliases.zsh
@@ -0,0 +1,14 @@
+alias json="python -mjson.tool"
+alias grpe="grep"
+alias vl=vimpager
+alias diff="diff --suppress-common-lines --side-by-side"
+alias ws="cd $WS"
+alias fs="cd $FS"
+alias gh="cd $GOHOME"
+alias ssh='TERM=xterm ssh'
+alias vagrant="TERM=xterm vagrant"
+alias t="todo.sh"
+alias gd="git diff --color"
+alias gdc="git diff --color --cached"
+alias celar="clear"
+alias a=". venv/bin/activate"
--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -32,18 +32,3 @@ alias ll='ls -lh'
 alias la='ls -lAh'
 
 alias afind='ack-grep -il'
-
-alias json="python -mjson.tool"
-alias grpe="grep"
-alias vl=vimpager
-alias diff="diff --suppress-common-lines --side-by-side"
-alias ws="cd $WS"
-alias fs="cd $FS"
-alias gh="cd $GOHOME"
-alias ssh='TERM=xterm ssh'
-alias vagrant="TERM=xterm vagrant"
-alias t="todo.sh"
-alias gd="git diff --color"
-alias gdc="git diff --color --cached"
-alias celar="clear"
-alias a=". venv/bin/activate"
