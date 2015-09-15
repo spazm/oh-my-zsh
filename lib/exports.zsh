3fb7e2cfd3f817a683fe2a7e74c388bbd8213d13
Dhaivat Pandit <dhaivat.pandit@sojern.com>
2015-09-15 13:20:42 -0700

    Java 7 & 8 homes

--- lib/exports.zsh
+++ lib/exports.zsh
@@ -3,7 +3,8 @@
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
 export WS="$HOME/workspace"
 export FS="$HOME/funspace"
-export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home'
+export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home'
+export JAVA7_HOME='/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home'
 export JAVA6_HOME='/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home'
 export AWS_CREDENTIAL_FILE=$HOME/.aws/credential-file
 export AWS_EC2_REGION=us-west-2
