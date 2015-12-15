781b7473e2dcddad354bb0adfe480d37f39ae73b
Andrew Grangaard <granny-github@ofb.net>
2015-12-14 16:18:06 -0800

    exports: don't delete default PATH

--- lib/exports.zsh
+++ lib/exports.zsh
@@ -1,6 +1,6 @@
 ## all exports shall be declared here
 
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
+export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
 export WS="$HOME/workspace"
 export FS="$HOME/funspace"
 export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home'
@@ -9,8 +9,8 @@ export JAVA6_HOME='/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/H
 export AWS_CREDENTIAL_FILE=$HOME/.aws/credential-file
 export AWS_EC2_REGION=us-west-2
 export AWS_IAM_HOME=/usr/local/iamcli
-export EC2_HOME="/usr/local/ec2-api-tools/"
-export EC2_LOC="/usr/local/ec2-api-tools/"
+export EC2_HOME="/usr/local/ec2-api-tools"
+export EC2_LOC="/usr/local/ec2-api-tools"
 export GOROOT="/usr/local/go"
 export GOPATH="$FS/go"
 export GOHOME="$GOPATH/src/github.com/ceocoder"
