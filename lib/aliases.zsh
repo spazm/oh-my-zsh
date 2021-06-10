6298b05e9aa7b3f2d03d3e5eb253dedb1e022ab9
Andrew Grangaard <granny-github@ofb.net>
2021-06-10 15:49:57 -0700

    remove some useless zsh aliases

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -36,12 +36,12 @@ alias afind='ack-grep -il'
 alias json="python -mjson.tool"
 alias grpe="grep"
 alias vl=vimpager
-#alias diff="diff --suppress-common-lines --side-by-side"
-alias ws="cd $WS"
-alias fs="cd $FS"
-alias gh="cd $GOHOME"
-alias ssh='TERM=xterm ssh'
-alias vagrant="TERM=xterm vagrant"
+# alias diff="diff --suppress-common-lines --side-by-side"
+# alias ws="cd $WS"
+# alias fs="cd $FS"
+# alias gh="cd $GOHOME"
+# alias ssh='TERM=xterm ssh'
+# alias vagrant="TERM=xterm vagrant"
 alias t="todo.sh"
 alias gd="git diff --color"
 alias gdc="git diff --color --cached"
