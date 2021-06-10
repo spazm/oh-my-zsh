688ef6783d57ec5f3eb00d6a82a6cef72a9d03f1
Andrew Grangaard <granny-github@ofb.net>
2021-06-10 15:52:23 -0700

    comment out some unused zsh exports for java, go, etc

--- lib/exports.zsh
+++ lib/exports.zsh
@@ -1,20 +1,19 @@
 ## all exports shall be declared here
 
-export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
-export WS="$HOME/workspace"
-export FS="$HOME/funspace"
-export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home'
-export JAVA7_HOME='/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home'
-export JAVA6_HOME='/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home'
+#export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin
+#export WS="$HOME/workspace"
+#export FS="$HOME/funspace"
+#export JAVA_HOME='/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home'
+#export JAVA7_HOME='/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home'
+#export JAVA6_HOME='/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home'
 #export AWS_CREDENTIAL_FILE=$HOME/.aws/credential-file
 export AWS_EC2_REGION=us-west-2
-export AWS_IAM_HOME=/usr/local/iamcli
-export EC2_HOME="/usr/local/ec2-api-tools"
-export EC2_LOC="/usr/local/ec2-api-tools"
-export PATH=$AWS_IAM_HOME/bin:$EC2_LOC/bin:$PATH
+#export AWS_IAM_HOME=/usr/local/iamcli
+#export EC2_HOME="/usr/local/ec2-api-tools/"
+#export EC2_LOC="/usr/local/ec2-api-tools/"
+#export PATH=$AWS_IAM_HOME/bin:$EC2_LOC/bin:$PATH:$GOROOT/bin:$GOPATH/bin
 export AIRSHIP_SSH_COMMAND=/usr/local/bin/as-ssh
 # only useful with CLANG.  Which we only use on OSX
 # export CFLAGS=-Qunused-arguments
 # export CPPFLAGS=-Qunused-arguments
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-alias gh="cd $GOHOME"
